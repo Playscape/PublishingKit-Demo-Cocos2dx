223065867a0a2105a64378a4920fade06b4d266a
maximr <maximr@svn.playscape.com>
2014-03-25 16:44:34 +0000

    cocos2d-x: + build process android will copy Pushwoosh.jar to the relevant dir. + fixed pushwooshx in native code + demo app succesfully functioned with pushwoosh.

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -29,8 +29,20 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes $(LOCAL_PATH)/../../AppWarpX
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_curl_static cocos_extension_static
+
+# Playscape libs we're dependant on + gnustl shared lib
+LOCAL_SHARED_LIBRARIES := playscape_pubkit_shared playscape_pubkit_cocos2dx_shared gnustl_shared
             
 include $(BUILD_SHARED_LIBRARY)
+
+# Required path, playscape publishing kit modules reside there.
+$(call import-add-path,$(LOCAL_PATH)/../../../PlayscapePubkitCocos2D-X/proj.android/prebuilt)
+
+# Playscape Publishing Kit Modules
+$(call import-module,playscape_pubkit)
+$(call import-module,playscape_pubkit_cocos2dx)
+
+# Cocos2D-X Modules
 
 $(call import-module,libcurl)
 $(call import-module,CocosDenshion/android)
