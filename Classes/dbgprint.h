ae4f681205a0a7d8fde75a1c04e6bdbc95caf5c8
maximr <maximr@svn.playscape.com>
2014-03-27 07:22:35 +0000

    cocos2d-x PW support in ios

--- Classes/dbgprint.h
+++ Classes/dbgprint.h
@@ -5,7 +5,7 @@
 		#include <android/log.h>
 		#define dbgprint(...) __android_log_print(ANDROID_LOG_INFO, "ps_native", __VA_ARGS__);
 	#else
-		#define dbgprint(...) printf(__VA_ARGS__);
+		#define dbgprint(...) printf(__VA_ARGS__);printf("\n");
 	#endif
 
 #endif
