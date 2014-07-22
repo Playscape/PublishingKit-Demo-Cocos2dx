d3ce798a812ef602b7b74f91a6f9d94726f4deb8
maximr <maximr@svn.playscape.com>
2014-07-22 13:45:53 +0000

    cocos2d-x Fixed imports

--- proj.android/src/com/playscape/publishingkit/PlayscapeActivity.java
+++ proj.android/src/com/playscape/publishingkit/PlayscapeActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.playscape.publishingkit.ActivityLifeCycle;
+import com.playscape.lifecycle.ActivityLifeCycle;
 
 /**
  * Playscape's default implementation to an activity
